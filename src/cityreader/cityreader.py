244ab61775b7903cdb75ec5a3ba51a04aacff4b5
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-05-07 23:08:08 -0700

    it is printing out csv file

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,12 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
 
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,6 +26,19 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+    with open('cities.csv') as cities_csv:
+        csv_reader = csv.reader(cities_csv, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print("HEADERS")
+                print(f"{row}\n")
+                line_count += 1
+            elif line_count <= 3:
+                print(row)
+                line_count += 1
+
+                
     
     return cities
 
